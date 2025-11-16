c72eed5aebcf812130fc1c6f5af0622b8e5fa519
Divyansh tripathi <divyanshtripathi48762@gmail.com>
2025-11-17 03:59:18 +0530

    Normalize image file extensions in movie list
    Updated image file extensions to be consistent with uppercase letters.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const movies = [
-    { title: "Esha Murder Karmphal", genre: "Action", image: "image-1.jpg" },
-    { title: "Top Gear", genre: "Comedy", image:"image-2.jpg" },
-    { title: "Devils Double Next Level", genre: "Drama", image: "image-3.avif" },
-    { title: "X-Men 9 Apocalypse", genre: "Adventure", image:"image-4.jpeg" },
-    { title: "Dragon Curse The Hidden Treasure", genre: "Romance", image:"image-5.jpg" },
-    { title: "Mahavatar Narsimha", genre: "Thriller", image:'image-6.jpeg' },
-    { title: "Kalki", genre: "Fantasy", image:"image-7.jpeg" },
+    { title: "Esha Murder Karmphal", genre: "Action", image: "image-1.JPG" },
+    { title: "Top Gear", genre: "Comedy", image:"image-2.JPG" },
+    { title: "Devils Double Next Level", genre: "Drama", image: "image-3.AVIF" },
+    { title: "X-Men 9 Apocalypse", genre: "Adventure", image:"image-4.JPEG" },
+    { title: "Dragon Curse The Hidden Treasure", genre: "Romance", image:"image-5.JPG" },
+    { title: "Mahavatar Narsimha", genre: "Thriller", image:'image-6.JPEG' },
+    { title: "Kalki", genre: "Fantasy", image:"image-7.JPEG" },
     { title: "Tumbbad", genre: "Horror", image:"image-8.JPG" }
 ];
 
